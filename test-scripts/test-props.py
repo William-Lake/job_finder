21c6f7478d9cca0e14e31ab3f4912182561c845c
ki7mt <ki7mt@yahoo.com>
2019-01-11 17:16:30 -0700

    move props script to test-scripts folder

---
+++ test-scripts/test-props.py
@@ -0,0 +1,38 @@
+""" Simple script to test SQLite3 props table"""
+
+import os
+from os.path import expanduser
+import sys
+import sqlite3
+
+def get_user_home():
+    """Return User Home Directory"""
+    return expanduser("~")
+
+# Comment / Uncomment the DB paths below based on yoru system
+
+# For Windows
+db = get_user_home() + "\AppData\Local\job_finder\jobfinder.db"
+
+# For Linux
+# db = get_user_home() + "/.local/share/job_finder/jobfinder.db"
+
+# No edits required beyond this point.
+data = {}
+try:
+    conn = sqlite3.connect(db)
+    cur = conn.cursor()
+    cur.execute('SELECT * FROM props WRE ROWID = 1')
+    data = cur.fetchone()
+except sqlite3.OperationalError as sql3_error:
+    print("Failed fetchall on props => {sql3_error}".format(sql3_error=sql3_error))
+    sys.exit(2)
+
+print("\n")
+print(data)
+print("\n")
+print(data[0])
+print(data[1])
+print(data[2])
+
+sys.exit(0)
