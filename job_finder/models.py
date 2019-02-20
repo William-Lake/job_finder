1775d08c69742192eb2dbd00ec6fca1d19f5de9b
William Lake <>
2019-02-20 07:08:44 -0700

    Updated Models

--- job_finder/models.py
+++ job_finder/models.py
@@ -1,6 +1,6 @@
 from peewee import *
 
-database = PostgresqlDatabase('jobs', **{'host': '127.0.0.1', 'user': 'jobs_admin', 'password': 'jobs_admin'})
+database = PostgresqlDatabase('job_finder', **{'host': '127.0.0.1', 'user': 'jobs_admin', 'password': 'jobs_admin'})
 
 class UnknownField(object):
     def __init__(self, *_, **__): pass
@@ -16,28 +16,46 @@ class DatabaseInfo(BaseModel):
 
     class Meta:
         table_name = 'database_info'
+        schema = 'jobs'
 
 class Job(BaseModel):
-    contest_num = BigIntegerField()
+    content_num = IntegerField()
     date_closed = DateField(null=True)
     date_opened = DateField()
     dept = TextField()
-    site_id = BigIntegerField()
+    site_id = IntegerField()
     site_url = TextField()
     title = TextField()
 
     class Meta:
         table_name = 'job'
+        schema = 'jobs'
+
+class OpenJobView(BaseModel):
+    content_num = IntegerField(null=True)
+    date_closed = DateField(null=True)
+    date_opened = DateField(null=True)
+    dept = TextField(null=True)
+    id = IntegerField(null=True)
+    site_id = IntegerField(null=True)
+    site_url = TextField(null=True)
+    title = TextField(null=True)
+
+    class Meta:
+        table_name = 'open_job_view'
+        schema = 'jobs'
+        primary_key = False
 
 class Prop(BaseModel):
-    is_selected = BooleanField(constraints=[SQL("DEFAULT false")])
-    smtp = CharField()
-    port = IntegerField()
     email = CharField()
+    is_selected = BooleanField(constraints=[SQL("DEFAULT false")])
+    port = IntegerField()
     pword = CharField()
+    smtp = CharField()
 
     class Meta:
         table_name = 'prop'
+        schema = 'jobs'
 
 class Recipient(BaseModel):
     data_added = DateField()
@@ -45,4 +63,5 @@ class Recipient(BaseModel):
 
     class Meta:
         table_name = 'recipient'
+        schema = 'jobs'
 
